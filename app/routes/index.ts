a8efc2dd64db7886dba299d83441aad5997d1322
Rishav Anand <rishavdipsite@gmail.com>
2020-04-02 18:34:17 +0530

    add health route

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,11 +1,13 @@
-import { Router } from 'express';
-import UserRouter from './Users';
+import { Router } from "express";
+import UserRouter from "./Users";
+import HealthRouter from "./Health";
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
-router.use('/users', UserRouter);
+router.use("/users", UserRouter);
+router.use("/health", HealthRouter);
 
 // Export the base-router
 export default router;
