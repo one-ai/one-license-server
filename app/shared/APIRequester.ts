6c7e66219c2150142f95d88f6fde9a2a929817bd
Rishav Anand <rishavdipsite@gmail.com>
2020-04-07 14:46:06 +0530

    add api requester class

---
+++ app/shared/APIRequester.ts
@@ -0,0 +1,128 @@
+import fetch, { Response } from 'node-fetch';
+import logger from './Logger';
+
+export const enum APIMethods {
+    POST = 'POST',
+    GET = 'GET',
+    PUT = 'PUT',
+    DELETE = 'DELETE',
+}
+
+export type KeyValue<T, U> = {
+    key: T;
+    value: U;
+};
+
+export class APIRequester {
+    private url: string;
+    private headers: string[][];
+    private method: string;
+    private body: string | undefined;
+    private responsePlainText: string | undefined;
+    private STATUS_CODE: number | undefined;
+    private SUCCESS = false;
+    private requestPromise: Promise<Response> | null = null;
+
+    constructor(initialData: { url: string }) {
+        this.url = initialData.url;
+        this.headers = [];
+        this.method = '';
+    }
+
+    /**
+     * Adds headers to request
+     * @param headers - Array of headers
+     */
+    setHeaders(headers: KeyValue<string, string>[]): APIRequester {
+        for (const i in headers) {
+            if (headers[i].hasOwnProperty('key') && headers[i].hasOwnProperty('value')) {
+                this.headers.push([headers[i].key, headers[i].value]);
+            }
+        }
+        return this;
+    }
+
+    /**
+     * Add method to request
+     * @param newMethod - REST method
+     */
+    setMethod(newMethod: APIMethods): APIRequester {
+        this.method = newMethod;
+        return this;
+    }
+
+    /**
+     * Make request
+     * @param body - Key value pairs to send in body
+     */
+    request<T>(body: T): APIRequester {
+        this.body = JSON.stringify(body);
+        this.requestPromise = fetch(this.url, {
+            headers: this.headers,
+            method: this.method,
+            body: this.method === 'GET' ? undefined : JSON.stringify(body),
+        });
+        return this;
+    }
+
+    /**
+     * Extract and return response body in JSON format
+     */
+    async json<T>(): Promise<T> {
+        try {
+            const res: Response | null = await this.requestPromise;
+
+            if (res === null)
+                // When response is empty
+                throw new Error('fetch init failed');
+
+            this.STATUS_CODE = res.status;
+            this.responsePlainText = await res.text();
+
+            if (this.STATUS_CODE !== 200)
+                // When request is not successful
+                throw new Error('Request status code not 200');
+
+            this.SUCCESS = true;
+
+            return (await JSON.parse(this.responsePlainText)) as T;
+        } catch (err) {
+            return this.handleError<T>(err);
+        }
+    }
+
+    get statusCode() {
+        return this.STATUS_CODE;
+    }
+
+    get success() {
+        return this.SUCCESS;
+    }
+
+    /**
+     * Logs error and throws new error message for global error handle
+     * @param err - Request error
+     */
+    handleError<T>(err: Error): Promise<T> {
+        logger.error('URL: ' + this.url);
+        logger.error('Method: ' + this.method);
+        logger.error('Headers: ' + JSON.stringify(this.headers));
+        //logger.error('Body: ' + this.body);
+        logger.error('Status code: ' + this.STATUS_CODE);
+        logger.error('Plain text response: ' + this.responsePlainText);
+        logger.error('Request to remote service could not be made: ' + err.message);
+        throw err;
+    }
+}
+
+export class RequestBody<T> {
+    constructor(private body: T) {}
+
+    get requestBody(): T {
+        return this.body;
+    }
+
+    set requestBody(newRequestBody: T) {
+        this.body = newRequestBody;
+    }
+}
