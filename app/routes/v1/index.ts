94239597ca49488f126b47b224f5f26925bc5e48
Rishav Anand <rishavdipsite@gmail.com>
2020-04-28 09:13:33 +0530

    remove forced auth for all endpoints

--- app/routes/v1/index.ts
+++ app/routes/v1/index.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import { UserRouter } from './users';
 import { AccessRouter } from './access';
 import { HealthRouter } from './HealthRouter';
-import { AuthMiddleware } from '@controllers';
 import { ProductRouter } from './products';
 import { VersionRouter } from './versions';
 import { LicenseRouter } from './licenses';
@@ -13,9 +12,6 @@ const V1Router = Router({ mergeParams: true });
 // Add public sub-routes
 V1Router.use('/', AccessRouter);
 V1Router.use('/health', HealthRouter);
-
-// Insert token check
-V1Router.use(AuthMiddleware);
 
 // Add private sub-routes
 V1Router.use('/users', UserRouter);
